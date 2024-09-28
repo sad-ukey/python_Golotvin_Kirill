56f8f50368cf06aba89434ab97615f78eeebf057
sad-ukey <146202627+sad-ukey@users.noreply.github.com>
2024-09-28 20:59:40 +0300

    Update task5.py

--- week 2/task5.py
+++ week 2/task5.py
@@ -3,3 +3,5 @@ def isPalindrome(s):
     cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
     return cleaned_s == cleaned_s[::-1]
 
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
