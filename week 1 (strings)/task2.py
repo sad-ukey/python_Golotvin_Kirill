4a333d0948069f6720d86c5d56f1cf35a8c984ba
sad-ukey <146202627+sad-ukey@users.noreply.github.com>
2024-09-28 16:16:53 +0300

    .

--- week 1 (strings)/task2.py
+++ week 1 (strings)/task2.py
@@ -1,21 +1,20 @@
 def ifPolindromic(s):
     for i, j in zip(range(len(s) // 2), range(1, (len(s) // 2) + 1)):
         if s[i] != s[-j]:
-            return ''
+            return ""
     return s
 
 
-longestPolindrome = ''
+longestPolindrome = ""
 s = input()
 srez = 1
 
 for i in range(len(s)):
     for j in range(srez, len(s)):
         if s[i] == s[j]:
-            polindrome = ifPolindromic(s[i:j+1])
+            polindrome = ifPolindromic(s[i : j + 1])
             if len(longestPolindrome) < len(polindrome):
                 longestPolindrome = polindrome
     srez += 1
-    
-print(longestPolindrome)        
 
+print(longestPolindrome)
--- week 1 (strings)/task3.py
+++ week 1 (strings)/task3.py
@@ -1,13 +1,13 @@
-numbers =  {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-           }
+numbers = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
 
 digits = input()
 letter_list = list(digits)
@@ -20,8 +20,8 @@ for i in range(len(letter_list)):
     for j in range(srez, len(letter_list)):
         int_j = int(letter_list[j])
         str_j = letter_list[j]
-        print(i, '', j, int_i, '', int_j)
-        #for ii in numbers
-        #final_list.append(numbers[i]+)
-        
+        print(i, "", j, int_i, "", int_j)
+        # for ii in numbers
+        # final_list.append(numbers[i]+)
+
     srez += 1
