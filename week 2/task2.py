7d59abde3ea50cbad11a6392d3696a1c5cfa4799
sad-ukey <146202627+sad-ukey@users.noreply.github.com>
2024-10-05 21:00:58 +0300

    .

--- week 2/task2.py
+++ week 2/task2.py
@@ -1,13 +1,13 @@
 def func(haystack, needle):
-    
     if len(haystack) < len(needle):
         return -1
-    
-    for i, j in zip(range(0, len(haystack)-len(needle)+1), range(len(needle)-1, len(haystack))):
-        if haystack[i:j+1] == needle:
+
+    for i, j in zip(range(0, len(haystack) - len(needle) + 1), range(len(needle) - 1, len(haystack))):
+        if haystack[i:j + 1] == needle:
             return i
-    
+
     return -1
+
 
 haystack = input()
 needle = input()
